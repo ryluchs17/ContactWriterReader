a19d283021827bbc4380246d83045762acd1900b
ryluchs17 <ryluchs17@saintjosephhs.com>
2015-12-05 18:17:01 -0500

    fixed it so it now reads the data into Contact objects before printing

--- src/ContactWriterReader.java
+++ src/ContactWriterReader.java
@@ -48,7 +48,7 @@ public class ContactWriterReader {
 				System.out.println("Name must not include / character");
 			}
 			
-			if (!line.matches("[ -~]+.txt")){
+			if (!line.matches("[ -~]+.txt")) {
 				System.out.println("Filename must end in .txt");
 			}
 		} while (!line.matches("[^/]+") || !line.matches("[ -~]+.txt"));
@@ -56,15 +56,60 @@ public class ContactWriterReader {
 		file = new File(line);
 		
 		// If the file exists, read it, if not, create it
-		if(file.exists()){
+		if (file.exists()) {
 			
 			read = new BufferedReader(new FileReader(file));
 			
 			System.out.printf("\n");
 			
-			while(read.ready()) {
-		        	System.out.println(read.readLine());
-		    }
+			// Read data into program
+			while (read.ready() && i < 256) {
+				
+				contacts[i] = new Contact();
+				
+				contacts[i].setLastName(read.readLine());
+				contacts[i].setFirstName(read.readLine());
+				contacts[i].setMiddleName(read.readLine());
+				contacts[i].setPrefix(read.readLine());
+				contacts[i].setPhone(read.readLine());
+				contacts[i].setEmail(read.readLine());
+				contacts[i].setStreet(read.readLine());
+				contacts[i].setCity(read.readLine());
+				contacts[i].setState(read.readLine());
+				contacts[i].setZip(read.readLine());
+				contacts[i].setOccupation(read.readLine());
+				read.skip(1L);
+				
+				i++;
+			}
+			
+			// fixes an indexing error in the file
+			i--;
+			
+			// fun facts
+			if (i > 150) {
+				System.out.printf("Did you know that the human brain can only comfortable maintain\n150 realtionaships at a time? It's called Dunbar's number.\n\n");
+			}
+			
+			// print data
+			for(int ctr = 0; ctr <= i; ctr++) {
+		        System.out.println("Last name: " + contacts[ctr].getLastName());
+		        System.out.println("First name: " + contacts[ctr].getFirstName());
+		        System.out.println("Middle name: " + contacts[ctr].getMiddleName());
+		        System.out.println("Prefix: " + contacts[ctr].getPrefix());
+		        System.out.println("Phone number: " + contacts[ctr].getPhone());
+		        System.out.println("Email address: " + contacts[ctr].getEmail());
+		        System.out.println("Street: " + contacts[ctr].getStreet());
+		        System.out.println("City: " + contacts[ctr].getCity());
+		        System.out.println("State: " + contacts[ctr].getState());
+		        System.out.println("ZIP code: " + contacts[ctr].getZip());
+		        System.out.println("Occupation: " + contacts[ctr].getOccupation());
+		        System.out.print('\n');
+			}
+			
+//			while(read.ready()) {
+//		        	System.out.println(read.readLine());
+//		    }
 			
 			read.close();
 			
@@ -73,7 +118,7 @@ public class ContactWriterReader {
 			write = new PrintWriter(file);
 			
 			// Get user input
-			while (keepWriting) {
+			while (keepWriting && i < 256) {
 				
 				// Create the current contact class
 				contacts[i] = new Contact();
@@ -163,7 +208,7 @@ public class ContactWriterReader {
 		        
 		        if (line.equals("y")) {
 		        	i++;
-		        	System.out.printf("\n");
+		        	System.out.print('\n');
 		        } else {
 		        	keepWriting = false;
 		        }
@@ -172,25 +217,27 @@ public class ContactWriterReader {
 			
 			// Write data to file
 			for(int ctr = 0; ctr <= i; ctr++) {
-		        write.println("Last name: " + contacts[ctr].getLastName());
-		        write.println("First name: " + contacts[ctr].getFirstName());
-		        write.println("Middle name: " + contacts[ctr].getMiddleName());
-		        write.println("Prefix: " + contacts[ctr].getPrefix());
-		        write.println("Phone number: " + contacts[ctr].getPhone());
-		        write.println("Email address: " + contacts[ctr].getEmail());
-		        write.println("Street: " + contacts[ctr].getStreet());
-		        write.println("City: " + contacts[ctr].getCity());
-		        write.println("State: " + contacts[ctr].getState());
-		        write.println("ZIP code: " + contacts[ctr].getZip());
-		        write.println("Occupation: " + contacts[ctr].getOccupation());
+		        write.println(contacts[ctr].getLastName());
+		        write.println(contacts[ctr].getFirstName());
+		        write.println(contacts[ctr].getMiddleName());
+		        write.println(contacts[ctr].getPrefix());
+		        write.println(contacts[ctr].getPhone());
+		        write.println(contacts[ctr].getEmail());
+		        write.println(contacts[ctr].getStreet());
+		        write.println(contacts[ctr].getCity());
+		        write.println(contacts[ctr].getState());
+		        write.println(contacts[ctr].getZip());
+		        write.println(contacts[ctr].getOccupation());
 		        write.print('\n');
 			}
+			
+			//write.println("[END OF LIST]");
 			
 			// Close the file
 			write.close();
 			
 			// Thank the user
-			System.out.printf("\nThank you! Your Contact information has been saved!");
+			System.out.printf("\nThank you! Your Contact information has been saved!\n");
 			
 		}
 
